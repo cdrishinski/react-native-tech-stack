d123c921a54d9cddc82a562648b1822328712346
cdrishinski <cdrishinski@gmail.com>
2019-06-20 14:03:32 -0600

    final code for project

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const selectLibrary = (libraryID) => {
-    return {
-        type: 'select_library',
-        payload: libraryID
-    }
-
-}
--- src/components/common/index.js
+++
@@ -1,9 +0,0 @@
-import { Spinner } from './Spinner';
-
-// This * imports and exports these components simultaneously
-export * from './Button';
-export * from './Card';
-export * from './CardSection';
-export * from './Header';
-export * from './Input';
-export * from './Spinner'
