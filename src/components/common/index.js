c7c1682db841a201fd9533b05b0eebbaa28101f1
cdrishinski <cdrishinski@gmail.com>
2019-06-20 10:34:50 -0600

    added header and other common components to app

---
+++ src/components/common/index.js
@@ -0,0 +1,9 @@
+import { Spinner } from './Spinner';
+
+// This * imports and exports these components simultaneously
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner'
